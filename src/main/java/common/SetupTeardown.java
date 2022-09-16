0e585cab38dcc9e02d5e60500656fa509e8aace6
moctar <moctar.diaw.fr@gmail.com>
2022-09-16 16:08:21 +0200

    create client java api rest

--- src/main/java/common/SetupTeardown.java
+++ src/main/java/common/SetupTeardown.java
@@ -1,9 +1,15 @@
 package main.java.common;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
 public class SetupTeardown {
 
     protected WebDriver drivers;
@@ -22,7 +28,29 @@ public class SetupTeardown {
     }
 
     @AfterMethod
-    public void teardown(){
+    public void teardown(ITestResult result) throws IOException, NoSuchAlgorithmException, KeyStoreException, InterruptedException, KeyManagementException {
+
+        ImportResultsToXray res = new ImportResultsToXray();
+
+        if(result.getStatus() == ITestResult.SUCCESS)
+        {
+            System.out.println("passed **");
+            res.generateJsonResults("PASSED");
+
+
+        }
+        else if(result.getStatus() == ITestResult.FAILURE)
+        {
+            System.out.println("Failed **");
+            res.generateJsonResults("FAILED");
+
+        }
+        else if(result.getStatus() == ITestResult.SKIP ){
+
+            System.out.println("Skiped**");
+            res.generateJsonResults("SKIPED");
+        }
+        res.RemonteResultats();
         drivers.quit();
     }
 }
